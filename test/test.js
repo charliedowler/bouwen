aba0e15dc071ab6ef180f889175c519e4f4aa2aa
charliedowler <charliedowler15@gmail.com>
2014-09-03 19:10:01 +0100

    updated tests

--- test/test.js
+++ test/test.js
@@ -3,11 +3,11 @@ var assert = require('assert');
 var isUp = require('is-up');
 var exec = require('child_process').exec;
 
-it('Should see that the bouwen build is unknown', function(done) {
+it('Should see that the bouwen build is STARTED', function(done) {
   isUp('google.com', function(err, up) {
     if (err || !up) done();
     exec('node ./bin/bouwen -j https://api.travis-ci.org/repositories/charliedowler/bouwen', function(err, stdout, stderr) {
-      assert.equal(stdout, 'Unknown\n');
+      assert.equal(stdout, 'STARTED\n');
       done()
     })
   })
