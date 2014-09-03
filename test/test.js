445f12cad30812d7773cd4285367aa877f357a01
charliedowler <charliedowler15@gmail.com>
2014-09-03 19:08:01 +0100

    Setup tests

---
+++ test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+var assert = require('assert');
+var isUp = require('is-up');
+var exec = require('child_process').exec;
+
+it('Should see that the bouwen build is unknown', function(done) {
+  isUp('google.com', function(err, up) {
+    if (err || !up) done();
+    exec('node ./bin/bouwen -j https://api.travis-ci.org/repositories/charliedowler/bouwen', function(err, stdout, stderr) {
+      assert.equal(stdout, 'Unknown\n');
+      done()
+    })
+  })
+});
