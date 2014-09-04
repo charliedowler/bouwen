414e97e0ecebada0ff235d081c83495146118f00
charliedowler <charliedowler15@gmail.com>
2014-09-04 19:35:08 +0100

    Fixed tests to run locally

--- test/test.js
+++ test/test.js
@@ -1,14 +1,16 @@
 'use strict';
 var assert = require('assert');
 var isUp = require('is-up');
+var isTravis = require('is-travis');
 var exec = require('child_process').exec;
 
-it('Should see that the bouwen build is STARTED', function(done) {
+it('Should see that the bouwen build is either STARTED | PASSED | FAILED', function(done) {
   isUp('google.com', function(err, up) {
     if (err || !up) done();
     exec('node ./bin/bouwen -j https://api.travis-ci.org/repositories/charliedowler/bouwen', function(err, stdout, stderr) {
-      assert.equal(stdout, 'STARTED\n');
-      done()
+      var status = (/PASSED/.test(stdout)) ? 'PASSED\n' : 'FAILED\n';
+      assert.equal(stdout, isTravis ? 'STARTED\n' : status);
+      done();
     })
   })
 });
